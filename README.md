b50af83e2937fbc92b79224d59cf719114149625
yongjulejule <lyjshow200@gmail.com>
2022-03-23 11:37:19 +0900

    feat : build tiny server

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # simple-server
+
 소켓 가지고 노는 레포
+
+# Uses
+
+`nc hostname(IP) port` -> 해당 포트에 접속해보는 command
+
+서버 실행
+
+```
+./server port
+```
+
+요청 보내기
+
+```
+nc [ip] [port]
+```
+
+![output](./asset/output.png)
+
+서버 File descriptor
+![server](./asset/server.png)
+
+클라이언트 File descriptor
+![client](./asset/client.png)
---
+++ src/server.cpp
@@ -0,0 +1,77 @@
+/**
+ * @file server.c
+ * @author yongjule (lyjshow200@gmail.com)
+ * @brief simple server
+ * @version 0.1
+ * @date 2022-03-23
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <arpa/inet.h>
+#include <sys/errno.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <iostream>
+
+bool isNumbers(std::string str) {
+  return !str.empty() &&
+         std::find_if(str.begin(), str.end(), [](char c) -> bool {
+           return !std::isdigit(c);
+         }) == str.end();
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2 || !isNumbers(argv[1])) {
+    std::cerr << "Usage: " << argv[0] << " <port>\n";
+    exit(EXIT_FAILURE);
+  }
+
+  // create a socket (TCP, IPv4). check /etc/protocols
+  int socketFd = socket(AF_INET, SOCK_STREAM, 0);
+  if (socketFd == -1) {
+    std::cerr << "socket() failed: " << std::strerror(errno) << "\n";
+    exit(EXIT_FAILURE);
+  }
+
+  sockaddr_in serverAddr;
+  memset(&serverAddr, 0, sizeof(serverAddr));
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(atoi(argv[1]));
+  serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  // bind the socket to the IP address and port number
+  if (bind(socketFd, reinterpret_cast<sockaddr *>(&serverAddr),
+           sizeof(serverAddr)) == -1) {
+    std::cerr << "bind() failed: " << std::strerror(errno) << "\n";
+    exit(EXIT_FAILURE);
+  }
+
+  // mark the socket as a passive socket (대기상태)
+  if (listen(socketFd, 42) == -1) {
+    std::cerr << "listen() failed: " << std::strerror(errno) << "\n";
+    exit(EXIT_FAILURE);
+  }
+
+  sockaddr_in clientAddr;
+  socklen_t clientAddrLen = sizeof(clientAddr);
+
+  // extract request from client and fill info to clientAddr. Finally return new
+  // socket fd to communicate with client
+  int connectionFd = accept(socketFd, reinterpret_cast<sockaddr *>(&clientAddr),
+                            &clientAddrLen);
+  if (connectionFd == -1) {
+    std::cerr << "accept() failed: " << std::strerror(errno) << "\n";
+    exit(EXIT_FAILURE);
+  }
+
+  write(connectionFd, "Hello, Client!\n", 15);
+  write(1, "Hello, Server!\n", 15);
+
+  close(connectionFd);
+  close(socketFd);
+
+  return EXIT_SUCCESS;
+}
